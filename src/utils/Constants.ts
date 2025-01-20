c56e768f0c3bcdedd16843b2c2ea1f8a92be5c28
almakhanov <nur.almakhanov@gmail.com>
2025-01-20 22:30:08 +0500

    new roles

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -6,5 +6,7 @@ export interface RoleOption {
 export const ROLE_OPTIONS: RoleOption[] = [
     { text: "Owner", value: "owner" },
     { text: "РОП", value: "branch_director" },
-    { text: "Менеджер", value: "sales_manager" }
+    { text: "Менеджер", value: "sales_manager" },
+    { text: "Company Viewer", value: "company_viewer" },
+    { text: "Branch Viewer", value: "branch_viewer" }
 ];
